94029828245f01232bbe3a6ddf3004e86f8c5723
softmodded <evelyn@yandhi.org>
2026-06-07 15:12:45 -0400

    add track button support: press track btn lights corresponding LED until released

--- app/src/main.c
+++ app/src/main.c
@@ -1,24 +1,32 @@
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/adc.h>
 #include <soc.h>
 
-#define NRF_P0_OUT (*(volatile uint32_t *)0x50000504)
-#define NRF_P1_OUT (*(volatile uint32_t *)0x50000804)
+#define ADC_NODE DT_NODELABEL(adc)
+#define LADDER_CH 0
 
 struct led { NRF_GPIO_Type *port; uint32_t pin; };
-static const struct led leds[] = {
+static const struct led pb_leds[] = {
 	{ NRF_P1, 13 }, { NRF_P0, 0 }, { NRF_P1, 12 }, { NRF_P0, 1 },
 };
-#define NUM_LEDS (sizeof(leds) / sizeof(leds[0]))
 
-static void all_off(void)
+static const struct led track_leds[] = {
+	{ NRF_P0, 29 }, { NRF_P0, 26 }, { NRF_P1, 15 }, { NRF_P1, 14 },
+};
+
+#define NUM_PB_LEDS   (sizeof(pb_leds) / sizeof(pb_leds[0]))
+#define NUM_TRK_LEDS  (sizeof(track_leds) / sizeof(track_leds[0]))
+
+static void all_pb_off(void)
 {
-	for (int i = 0; i < NUM_LEDS; i++)
-		leds[i].port->OUTCLR = (1 << leds[i].pin);
+	for (int i = 0; i < NUM_PB_LEDS; i++)
+		pb_leds[i].port->OUTCLR = (1 << pb_leds[i].pin);
 }
 
-static void set_on(int i)
+static void set_pb_on(int i)
 {
-	leds[i].port->OUTSET = (1 << leds[i].pin);
+	pb_leds[i].port->OUTSET = (1 << pb_leds[i].pin);
 }
 
 static void feed_wdt(void)
@@ -33,25 +41,51 @@ static void delay_50ms(void)
 		__ASM volatile ("nop");
 }
 
+static int read_ladder(const struct device *adc_dev)
+{
+	int16_t val = 0;
+	struct adc_sequence seq = {
+		.channels = BIT(LADDER_CH),
+		.buffer = &val,
+		.buffer_size = sizeof(val),
+		.resolution = 12,
+		.oversampling = 4,
+	};
+	if (adc_read(adc_dev, &seq) < 0) return -1;
+	return val;
+}
+
 int main(void)
 {
+	const struct device *adc_dev = DEVICE_DT_GET(ADC_NODE);
+
 	NRF_P0->PIN_CNF[27] = (GPIO_PIN_CNF_DIR_Input    << GPIO_PIN_CNF_DIR_Pos)   |
 			       (GPIO_PIN_CNF_PULL_Pullup << GPIO_PIN_CNF_PULL_Pos) |
 			       (GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos);
 
-	for (int i = 0; i < NUM_LEDS; i++)
-		leds[i].port->PIN_CNF[leds[i].pin] =
+	for (int i = 0; i < NUM_PB_LEDS; i++)
+		pb_leds[i].port->PIN_CNF[pb_leds[i].pin] =
 			(GPIO_PIN_CNF_DIR_Output   << GPIO_PIN_CNF_DIR_Pos)   |
 			(GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos) |
 			(GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos);
 
-	all_off();
+	for (int i = 0; i < NUM_TRK_LEDS; i++)
+		track_leds[i].port->PIN_CNF[track_leds[i].pin] =
+			(GPIO_PIN_CNF_DIR_Output   << GPIO_PIN_CNF_DIR_Pos)   |
+			(GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos) |
+			(GPIO_PIN_CNF_INPUT_Connect << GPIO_PIN_CNF_INPUT_Pos);
+
+	all_pb_off();
+	for (int i = 0; i < NUM_TRK_LEDS; i++)
+		track_leds[i].port->OUTCLR = (1 << track_leds[i].pin);
 
 	int pos = 0, dir = 1;
 
 	while (1) {
 		if (!(NRF_P0->IN & (1 << 27))) {
-			all_off();
+			all_pb_off();
+			for (int i = 0; i < NUM_TRK_LEDS; i++)
+				track_leds[i].port->OUTCLR = (1 << track_leds[i].pin);
 			feed_wdt();
 			NRF_POWER->RESETREAS = 0xFFFFFFFF;
 			NRF_POWER->SYSTEMOFF = 1;
@@ -59,19 +93,41 @@ int main(void)
 			for (;;);
 		}
 
-		all_off();
-		set_on(pos);
+		int ladder = device_is_ready(adc_dev) ? read_ladder(adc_dev) : -1;
+		int pressed = 0;
+		if (ladder >= 0) {
+			if (ladder < 100)      pressed = 0;
+			else if (ladder < 800)  pressed = 1;
+			else if (ladder < 1500) pressed = 2;
+			else if (ladder < 2200) pressed = 3;
+			else if (ladder < 3000) pressed = 4;
+		}
+
+		for (int i = 0; i < NUM_TRK_LEDS; i++) {
+			if (pressed == i + 1)
+				track_leds[i].port->OUTSET = (1 << track_leds[i].pin);
+			else
+				track_leds[i].port->OUTCLR = (1 << track_leds[i].pin);
+		}
+
+		all_pb_off();
+		if (!pressed) set_pb_on(pos);
+
 		*(volatile uint32_t *)0x2000FFF0 = (NRF_P0->OUT & 0xFFFF) | ((NRF_P1->OUT & 0xFFFF) << 16);
 		delay_50ms();
 		delay_50ms();
 		feed_wdt();
 
-		pos += dir;
-		if (pos == NUM_LEDS - 1) dir = -1;
-		if (pos == 0)             dir = 1;
+		if (!pressed) {
+			pos += dir;
+			if (pos == NUM_PB_LEDS - 1) dir = -1;
+			if (pos == 0)               dir = 1;
+		}
 
 		if (!(NRF_P0->IN & (1 << 27))) {
-			all_off();
+			all_pb_off();
+			for (int i = 0; i < NUM_TRK_LEDS; i++)
+				track_leds[i].port->OUTCLR = (1 << track_leds[i].pin);
 			feed_wdt();
 			NRF_POWER->RESETREAS = 0xFFFFFFFF;
 			NRF_POWER->SYSTEMOFF = 1;
